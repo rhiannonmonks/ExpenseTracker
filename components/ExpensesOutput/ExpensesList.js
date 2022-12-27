bdae6b8924656cf55d1f39aa9934aa83d053f7fc
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-12-27 18:59:48 +0000

    outputting a list of expenses

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,17 @@
+import { Text, FlatList } from "react-native";
+
+function renderExpenseItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
+
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+
+export default ExpensesList;
---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,47 @@
+import { View } from "react-native";
+import ExpensesList from "./ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e3",
+    description: "A pair of shorts",
+    amount: 50.99,
+    date: new Date("2021-10-15"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2022-02-01"),
+  },
+  {
+    id: "e5",
+    description: "A book",
+    amount: 18.99,
+    date: new Date("2022-05-01"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
