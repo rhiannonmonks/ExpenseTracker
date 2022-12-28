f07b453afc89c7b40e91fd728588440b9c836142
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-12-28 11:41:34 +0000

    formatting dates and expense list items

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,12 @@
-import { Text, FlatList } from "react-native";
+import { FlatList } from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData) {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
+  // note here this is prop spreading as the itemData props in our ExpenseItem component
+  // are the same as the ones we want to inject here (from the dummy data, we will also get id)
+  // the same line above could have been written by:
+  // return <ExpenseItem description={..description} amount={...amount} date={...date}/>
 }
 
 function ExpensesList({ expenses }) {
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,30 @@ const DUMMY_EXPENSES = [
     amount: 18.99,
     date: new Date("2022-05-01"),
   },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e8",
+    description: "A pair of shorts",
+    amount: 50.99,
+    date: new Date("2021-10-15"),
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2022-02-01"),
+  },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -50,7 +74,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 0,
+    paddingTop: 24,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
