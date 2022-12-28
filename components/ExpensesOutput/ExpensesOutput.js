225cbdadf9e5786314f5f09ef600e9bf0085e5c4
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-12-28 16:27:05 +0000

    using the context API from inside components

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,68 +3,11 @@ import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.29,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e3",
-    description: "A pair of shorts",
-    amount: 50.99,
-    date: new Date("2021-10-15"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2022-02-01"),
-  },
-  {
-    id: "e5",
-    description: "A book",
-    amount: 18.99,
-    date: new Date("2022-05-01"),
-  },
-  {
-    id: "e6",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e7",
-    description: "A pair of trousers",
-    amount: 89.29,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e8",
-    description: "A pair of shorts",
-    amount: 50.99,
-    date: new Date("2021-10-15"),
-  },
-  {
-    id: "e9",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2022-02-01"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
